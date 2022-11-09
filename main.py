5eb129f65c52c23c493073b233ebb00a4ba7ebb9
HaxTwoWater <haxtwowater@gmail.com>
2022-11-09 20:09:06 +0100

    debut

---
+++ main.py
@@ -0,0 +1,35 @@
+#DEBUT
+
+#On admet la fonction random qui retourne un nombre aléatoire entre 1 et 2
+#On admet la fonction input qui prend en paramètre les entrées utilisateurs que nous pouvons assigner a des variables
+
+#Definir la classe pfcGame
+
+    #Definir la fonction intro
+        #Afficher le retour de l'execution de la fonction print avec pour parametre "Bienvenue sur le Pierre Feuille Ciseau"
+        #Afficher le retour de l'execution de la fonction print avec pour parametre "Tapez 1 pour voir les règles | tapez 2 pour choisir le mode de jeu et la difficulté"
+        #Assigner a x la valeur input
+        #Essaie de la structure conditonelle suivante
+            #Switch de x
+                #cas de x est égal a 1
+                    #Alors retour de l'execution de la fonction gameRule
+                #cas de x est égal a 2
+                    #Alors retour de l'execution de la fonction gameMode
+                #autre cas
+                    #Alors 
+                    #Afficher le retour de l'execution de la fonction print avec pour parametre "Mauvais chiffre n'oubliez pas 1 ou 2, retour à l'intro"
+                    #Retour de l'execution de la fonction intro
+        #Exception en cas d'erreur de l'essaie
+            #Afficher le retour de l'execution de la fonction print avec pour parametre "Erreur mauvais type de valeur rentrer"
+            #Retour de l'execution de la fonction intro
+
+    #Definir la fonction gameRule
+        #Afficher le retour de l'execution de la fonction print avec pour parametre ""
+
+    #Definir la fonction gameMode
+
+    #Definir la fonction gameDiff
+
+    #Definir la fonction gameStart avec comme parametre x quelconque 
+
+#FIN
